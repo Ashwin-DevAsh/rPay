43aae09358320a823b78211342ace3593f6407ab
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-11 22:17:18 +0530

    storemerchant

--- app/src/main/java/com/DevAsh/recwallet/Helper/MerchantHelper.kt
+++ app/src/main/java/com/DevAsh/recwallet/Helper/MerchantHelper.kt
@@ -25,7 +25,7 @@ object MerchantHelper {
                             var merchantTemp = ArrayList<Merchant>()
                             for(i in 0 until response.length()){
                                 val user = Merchant(
-                                    response.getJSONObject(i)["storeName"].toString()
+                                    response.getJSONObject(i)["storename"].toString()
                                     ,"+"+response.getJSONObject(i)["number"].toString()
                                     ,response.getJSONObject(i)["id"].toString()
                                     ,response.getJSONObject(i)["email"].toString()
